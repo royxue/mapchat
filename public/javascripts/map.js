2d21dbdbd4f8a85b83c54ff40b6ea51129a50864
Lijun(Roy) Xue <xljroy@gmail.com>
2016-07-15 19:26:03 -0700

    Add map to index

---
+++ public/javascripts/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoicm95eHVlIiwiYSI6IjlkZjFkYTRiYzdlYTE5MGQyYjgzZjU5OWI0ZjhiNzM3In0.69Z8Hfaju1RfHIQaTKojcg';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-122.0738, 37.422],
+    zoom: 15
+});
